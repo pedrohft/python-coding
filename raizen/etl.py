2783b9f363209f441a0a1055e4d45899c502a1f5
Pedro Henrique Faria Teixeira <pedro.teixeira@ibm.com>
2022-02-16 18:03:37 -0300

    Extract pivot caches

--- raizen/etl.py
+++ raizen/etl.py
@@ -1,9 +1,16 @@
-from pyspark.sql import SparkSession
-import pandas
+# Your code here
+import pandas as pd
 
-spark = SparkSession.builder.appName("Test").getOrCreate()
+df = pd.read_excel(r'.\data\vendas-combustiveis-m3.xls', sheet_name="Derivative")
+df_diesel = pd.read_excel(r'.\data\vendas-combustiveis-m3.xls', sheet_name="Diesel")
 
-pdf = pandas.read_excel('excelfile.xlsx', sheet_name='sheetname', inferSchema='true')
-df = spark.createDataFrame(pdf)
+group_derivative = df.groupby(['ESTADO','COMBUSTÍVEL']) # Sales of oil derivative fuels by UF and product
 
-df.show()
+#Sales of diesel by UF and type
+# print(group_one.first())
+
+group_diesel = df_diesel.groupby(['ESTADO','COMBUSTÍVEL'])
+
+diesel_year_month = df_diesel.loc[df_diesel['ANO'].isin([2013])]
+
+print(diesel_year_month.stack())
