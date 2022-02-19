3c01d0780e1f8aae183e174817eb71ff3295938b
pedrohft <otmpedrofaria@gmail.com>
2022-02-19 17:11:31 -0300

    Adding files to database folder.

--- raizen/infrastructure/s3_file.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_s3_bucket_object" "job" {
-  bucket = aws_s3_bucket.dl.id
-  key    = "glue/pyspark/job_spark_from_tf.py"
-  acl    = "private"
-  source = "../job_spark.py"
-  etag   = filemd5("../job_spark.py")
-}
